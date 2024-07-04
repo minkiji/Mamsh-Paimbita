b92f40010014df6784b4c8aed28905cbe170052c
Arra <145094087+minkiji@users.noreply.github.com>
2024-07-05 00:12:41 +0800

    + contact page

--- index.js
+++ index.js
@@ -14,3 +14,41 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+
+
+
+const elements = document.querySelectorAll(".animate-load");
+
+function fadeInElements() {
+  let delay = 0;
+  elements.forEach((element, index) => {
+    setTimeout(() => {
+      element.classList.add("image--visible");
+    }, delay);
+    delay += 0; // Adjust the delay as needed
+  });
+}
+
+window.addEventListener("load", fadeInElements);
+
+
+
+// Fade in effects when scrolled
+const paragraphs = document.querySelectorAll(".animate-delay");
+
+document.addEventListener("scroll", function () {
+  paragraphs.forEach((paragraph) => {
+    if (isInView(paragraph)) {
+      paragraph.classList.add("paragraph--visible");
+    }
+  })
+})
+
+function isInView(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top <
+    (window.innerheight - 200 || document.documentElement.clientHeight - 200)
+  )
+}
